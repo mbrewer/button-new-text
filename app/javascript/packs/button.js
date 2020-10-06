1945e4385059ffe4c6574f7c9e1a0ce6e6d912bf
Mallory Brewer <hello@mallory.sh>
2020-10-05 23:40:23 -0400

    create sayHello function

--- app/javascript/packs/button.js
+++ app/javascript/packs/button.js
@@ -1,1 +1,9 @@
-alert('THANK YOU');
+function sayHello() { 
+  alert('Hello World!'); 
+}
+
+document.addEventListener('turbolinks:load', () => {  
+  const clickButton = document.getElementById("button-click");  
+
+  clickButton.addEventListener("click", sayHello); 
+});
