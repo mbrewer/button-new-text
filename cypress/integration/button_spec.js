350d7bfb9ae4c3af8425d5842d03b5ae280e1f5b
Mallory Brewer <hello@mallory.sh>
2020-10-08 23:23:50 -0400

    update passing cypress test

--- cypress/integration/button_spec.js
+++ cypress/integration/button_spec.js
@@ -2,6 +2,6 @@ describe('Verify text on click', () => {
   it('Click button, get text', () => {
     cy.visit('http://localhost:8080')
     cy.contains('Click Me').click()
-    cy.get('text').should('Hello', 'World')
+    cy.get('h1').should('contain', 'Hello World')
   })
 })
